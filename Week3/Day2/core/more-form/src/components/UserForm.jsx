7c454c9df84c4f8e3a0786e2ddb3a1d9d8161d8e
Asma Sahli <150928892+AsmaSahli@users.noreply.github.com>
2024-01-14 15:53:52 +0100

    commit

--- Week3/Day2/core/more-form/src/components/UserForm.jsx
+++ Week3/Day2/core/more-form/src/components/UserForm.jsx
@@ -5,27 +5,6 @@ const [lastName,setLastName] = useState("")
 const [email,setEmail] = useState("")
 const [password,setPassword] = useState("")
 const [confirm,setConfirm] = useState("")
-const [error ,setError] = useState("")
-const handleForm = (e) => {
-    if (e.target.name==='Firstname' && e.target.value.length < 2) {
-      setError("First name must be at least 2 characters");
-    }
-    else if (e.target.name==='Lastname' && e.target.value.length < 2) {
-      setError("Last name must be at least 2 characters");
-    }
-    else if (e.target.name==='Email' && e.target.value.length < 2) {
-      setError("Email must be at least 2 characters");
-    }
-    else if (e.target.name === 'password' && e.target.value.length < 8 && e.target.value !== confirm) {
-        setError("Passwords must be at least 8 characters and passwords must match");
-      }
-      
-
-    else {
-      setError("");
-    }
-    
-  };
 
 
 return (
@@ -33,36 +12,36 @@ return (
     <form > 
         <div >
                 <label htmlFor="firstName" className="form-label">First Name</label>
-                <input type="text" name="Firstname" className="form-control" value={firstName}  onChange={(e)=> {setFirstName(e.target.value) ;handleForm(e)}}/>
-                {
-                    firstName && error? <p>{error}</p>:''
-                }
+                <input type="text" name="Firstname" className="form-control" value={firstName}  onChange={(e)=> setFirstName(e.target.value) }/>
+                {firstName.length < 2 && firstName.length > 0 ? (
+                  <p>First Name must be at least 2 characters</p>
+          ) : " "
+          }
 
         </div>
         <div>                
             <label htmlFor="lastName" className="form-label">Last Name</label>
-            <input type="text"className="form-control"name='Lastname' value={lastName} onChange={(e)=>{setLastName(e.target.value); handleForm(e)}}/>
-            {
-                    lastName && error? <p>{error}</p>:''
-            }
+            <input type="text"className="form-control"name='Lastname' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+            {lastName.length < 2 && lastName.length > 0 ? (
+            <p>Last Name must be at least 2 characters</p>
+          ) : ""}
         </div>
         <div>
             <label htmlFor="email" className="form-label">Email</label>
-            <input type="text"className="form-control" name='Email' value={email} onChange={(e)=>{setEmail(e.target.value);handleForm(e)}} />
-            {
-                email && error? <p>{error}</p>:''
-            }
+            <input type="text"className="form-control" name='Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+            {email.length < 5 ? ( <p>Email must be at least 5 characters</p> ):" "}
         </div>
         <div>
             <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" name='password' value={password} onChange={(e)=>{setPassword(e.target.value); handleForm(e)}}/>
-            {
-                password && error? <p>{error}</p>:''
-            }
+            <input type="password" className="form-control" name='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            {password.length < 8 ? (
+            <p>Password must be at least 8 characters</p>
+          ) : ""}
         </div>
         <div>
             <label htmlFor="confirm" className="form-label">Confirm Password</label>
             <input type="password" className="form-control" value={confirm} onChange={(e)=>setConfirm(e.target.value)}/>
+            {confirm !== password ? <p>Passwords must match</p> : null}
         </div>
         <h6>Your Form data</h6>
     </form> 
