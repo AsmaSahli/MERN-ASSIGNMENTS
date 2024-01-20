8931a31f06b35c90d6176097ea14bc773b6cc7d3
Asma Sahli <150928892+AsmaSahli@users.noreply.github.com>
2024-01-20 21:37:30 +0100

    commit

--- Week4/Day2/Practice/pokemon/src/App.js
+++ Week4/Day2/Practice/pokemon/src/App.js
@@ -2,33 +2,28 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-
   const [pokemon, setPokemon] = useState([]);
 
-  useEffect(() => {
+  const fetchPokemon = () => {
     fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(response => response.json())
       .then(response => {
-
-        return response.json()
-      })
-      .then(response => {
-        setPokemon(response.results)
+        setPokemon(response.results);
       })
       .catch((err) => {
         console.log(err);
-      })
-    
+      });
+  };
 
-  }, []);  
+
 
   return (
-    <div className="App" >
+    <div className="App">
+      <button onClick={fetchPokemon}>Fetch Pokemon</button>
       <ul>
-        {
-          pokemon.map((pokemon, i)=>{
-            return (<li key={i}>{pokemon.name}</li>)
-          })
-        }
+        {pokemon.map((pokemon, i) => (
+          <li key={i}>{pokemon.name}</li>
+        ))}
       </ul>
     </div>
   );
